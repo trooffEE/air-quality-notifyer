517deac0a5a372c437ef07a7b32c6d8af8474fe0
Vadim <spicyn.v2001@gmail.com>
2024-04-29 19:02:59 +0600

    feat: added correct cron for script

--- main.go
+++ main.go
@@ -3,21 +3,17 @@ package main
 import (
 	"air-quality-notifyer/entity"
 	sensor "air-quality-notifyer/handlers"
-	"fmt"
 	"github.com/robfig/cron/v3"
 	"log"
 	"net/http"
 )
 
 func main() {
-	c := cron.New()
 	sensors := entity.NewSensorsData()
-	sensor.FetchSensorsData(&sensors)
-	fmt.Println("MAIN TEST", sensors)
-	//c.AddFunc("@every 1m", func() {
-	//	sensor.FetchSensorsData(&sensors)
-	//	fmt.Println("In Cron!", sensors)
-	//})
+	c := cron.New()
+	c.AddFunc("1 * * * * *", func() {
+		sensor.FetchSensorsData(&sensors)
+	})
 	c.Start()
 
 	err := http.ListenAndServe(":4000", nil)
