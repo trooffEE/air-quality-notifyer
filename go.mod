ae94fc962726bc64fa9faf9a4b79e1d0b2ba8fa3
Vadim <spicyn.v2001@gmail.com>
2024-08-18 01:50:02 +0700

    feat: started to add PostgresSQL to project

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 	github.com/robfig/cron/v3 v3.0.0
 )
--- main.go
+++ main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"air-quality-notifyer/bot"
 	"air-quality-notifyer/sensor"
+	_ "database/sql"
+	"fmt"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 func main() {
+	fmt.Println("TEST")
+	connString := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"))
+	db, err := sqlx.Connect("postgres", connString)
+	if err != nil {
+		log.Fatal(err, connString)
+	}
+	result, err := db.Exec("SELECT * from user_table")
+	if err != nil {
+		log.Fatal(err, 9)
+	}
+	fmt.Println(result, "test")
 	bot.InitTelegramBot().ListenForUpdates()
 	sensor.GetSensorsDataOnceIn("0 * * * *")
 }
