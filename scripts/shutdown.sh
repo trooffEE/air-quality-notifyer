199b74c97afdb635f013089f6eb287e19d42bfe3
Vadim Spitsyn <spicyn.v2001@gmail.com>
2025-09-25 16:49:26 +0700

    feat: updated docker config, removed startup.sh and shutdown.sh since bot dev environment now seperated. Also added technical debt about sensor invalidation to redis

--- scripts/shutdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ ! -z $DEVELOPMENT ]]; then
-  echo "DEVELOPMENT: Setting Webhook back"
-  curl "https://api.telegram.org/bot${TELEGRAM_SECRET}/setWebhook?url=https://${WEBHOOK_HOST}/webhook${TELEGRAM_SECRET}"
-fi
