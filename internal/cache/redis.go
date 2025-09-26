211e7df6ad46be895885fad2a652f1e85673de7a
Vadim Spitsyn <spicyn.v2001@gmail.com>
2025-09-26 19:46:30 +0700

    chore: removed config layer

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,14 +1,30 @@
 package cache
 
 import (
+	"os"
+
 	"github.com/redis/go-redis/v9"
 )
 
 func NewCacheClient() *redis.Client {
-	cfg := NewRedisConfig()
+	cfg := newRedisConfig()
 	return redis.NewClient(&redis.Options{
 		Addr:     cfg.Address,
 		Password: cfg.Password,
 		DB:       cfg.DBIndex,
 	})
+}
+
+type Config struct {
+	Address  string
+	Password string
+	DBIndex  int
+}
+
+func newRedisConfig() Config {
+	return Config{
+		Address:  os.Getenv("REDIS_HOST"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DBIndex:  0,
+	}
 }
