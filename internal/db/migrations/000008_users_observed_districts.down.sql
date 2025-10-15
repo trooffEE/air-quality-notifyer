9c1df85c98a5d1eda212bda5bf1bc15729a83695
Vadim Spitsyn <spicyn.v2001@gmail.com>
2025-10-15 11:03:06 +0700

    feat: added new migration

---
+++ internal/db/migrations/000008_users_observed_districts.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE users_observed_districts ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE users_observed_sensors ALTER COLUMN id DROP DEFAULT;
---
+++ internal/db/migrations/000008_users_observed_districts.up.sql
@@ -0,0 +1,27 @@
+-- users_observed_districts
+CREATE SEQUENCE users_observed_districts_id_seq;
+
+ALTER TABLE users_observed_districts
+    ALTER COLUMN id SET DEFAULT nextval('users_observed_districts_id_seq');
+
+-- Set the sequence value to the maximum current id + 1
+SELECT setval('users_observed_districts_id_seq', COALESCE(MAX(id), 0) + 1)
+FROM users_observed_districts;
+
+-- Make sure the id column is set as primary key (if not already)
+ALTER TABLE users_observed_districts
+    ADD PRIMARY KEY (id);
+
+-- users_observed_sensors
+CREATE SEQUENCE users_observed_sensors_id_seq;
+
+ALTER TABLE users_observed_sensors
+    ALTER COLUMN id SET DEFAULT nextval('users_observed_sensors_id_seq');
+
+-- Set the sequence value to the maximum current id + 1
+SELECT setval('users_observed_sensors_id_seq', COALESCE(MAX(id), 0) + 1)
+FROM users_observed_sensors;
+
+-- Make sure the id column is set as primary key (if not already)
+ALTER TABLE users_observed_sensors
+    ADD PRIMARY KEY (id);
